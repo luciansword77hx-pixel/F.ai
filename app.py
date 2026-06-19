c661146847b6e13aa994403bd5853dcaba1403fd
luciansword77hx-pixel <lucian.sword77hx@gmail.com>
2026-06-20 00:39:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ st.markdown(
         height: auto;
         z-index: 2;
         top: 130px; 
-        left: 41px; 
+        left: 55px; 
         transform-origin: top center;
         animation: swingLeftLeg 1.4s ease-in-out infinite; 
     }
